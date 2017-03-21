da6bff079e1589e599dff381df350aadfa568cdf
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2017-03-21 14:36:53 -0500

    nav, skills

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-var lista = $('#list-menu');
+$( document ).ready(function() {
+  // Main variables
+    var $aboutTitle = $('.about-myself .content h2');
+    var $developmentWrapper = $('.development-wrapper');
+    var developmentIsVisible = false;
+	
+/*--------------------------------- NAV*/
+	var lista = $('#list-menu');
 lista.children().click(clase);
 
 function clase(){
@@ -6,3 +13,41 @@ function clase(){
 	$(this).addClass('tab-current');
 	
 }
+/*---------------------------------- NAV*/
+	
+	$(window).scroll( function(){
+
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+/*###### SKILLS SECTION ######*/
+
+    var middle_of_developmentWrapper = $developmentWrapper.offset().top + $developmentWrapper.outerHeight()/2;
+
+    if((bottom_of_window > middle_of_developmentWrapper)&& (developmentIsVisible == false)){
+
+      $('.skills-bar-container li').each( function(){
+
+        var $barContainer = $(this).find('.bar-container');
+        var dataPercent = parseInt($barContainer.data('percent'));
+        var elem = $(this).find('.progressbar');
+        var percent = $(this).find('.percent');
+        var width = 0;
+
+        var id = setInterval(frame, 15);
+
+        function frame() {
+          if (width >= dataPercent) {
+              clearInterval(id);
+          } else {
+            width++;
+            elem.css("width", width+"%");
+            percent.html(width+" %");
+          }
+        }
+      });
+      developmentIsVisible = true;
+    }
+  }); 
+});
+	
+	
